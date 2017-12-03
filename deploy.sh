49b145aee03a903672d86ff9bc234a294cc015ff
bschwartzman <bschwartzman@gmail.com>
2017-12-03 17:21:23 -0500

    Updates and deply script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+###############
+# Deploy dotfiles to their locations
+# Makes folders if they're not present
+
+cp ./vimrc ~/.vimrc
+cp ./bashrc ~/.bashrc
+
+mkdir -p ~/.config
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
+cp ./compton.conf ~/.config/compton.conf
+cp ./config ~/.config/i3/config
+cp ./i3status-config ~/.config/i3status/config
